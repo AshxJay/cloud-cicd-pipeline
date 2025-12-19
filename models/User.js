f2e42e3d2c9bddc704619252e5cd4960da6b5a7a
AshxJay <ashutoshjairam.s2023cse@sece.ac.in>
2025-12-19 09:53:03 +0530

    encrypt test

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -19,5 +20,15 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// 🔐 HASH PASSWORD BEFORE SAVE
+userSchema.pre("save", async function (next) {
+  console.log("🔥 PRE-SAVE HOOK RUNNING");
+
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 module.exports = mongoose.model("User", userSchema);
